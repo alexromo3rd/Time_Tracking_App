11043b85695f7bce6c96cf845f2ffd853949333b
Alex Romo III <alexromo3rd@gmail.com>
2021-08-25 16:20:26 -0500

    Working on EditableTimer component

---
+++ public/js/app.js
@@ -0,0 +1,35 @@
+class TimerDashboard extends React.component {
+  render() {
+    return (
+      <div className='ui three column centered grid'>
+        <div className='column'>
+          <EditableTimerList />
+          <ToggleableTimerForm isOpen={true} />
+        </div>
+      </div>
+    );
+  }
+}
+
+class EditableTimerList extends React.component {
+  render() {
+    return (
+      <div id='timers'>
+        <EditableTimer
+          title='Learn React'
+          project='Web Domination'
+          elapsed='8986300'
+          runningSince={null}
+          editFormOpen={false}
+        />
+        <EditableTimer
+          title='Learn Extreme Ironing'
+          project='World Domination'
+          elapsed='3890985'
+          runningSince={null}
+          editFormOpen={true}
+        />
+      </div>
+    );
+  }
+}
